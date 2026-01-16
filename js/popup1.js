25a4c15ce022adde2a6760ce85259cbfb22ed96a
mikel-web <mikelodeyemi@gmail.com>
2026-01-16 08:41:17 +0100

    Add files via upload

---
+++ js/popup1.js
@@ -0,0 +1,106 @@
+let popup1  = document.getElementById("saving-popup1");
+        let popup2  = document.getElementById("saving-popup2");
+        let popup3  = document.getElementById("saving-popup3");
+        let popup4  = document.getElementById("saving-popup4");
+        let popup5  = document.getElementById("saving-popup-withdraw");
+        let profilePopup = document.getElementById("profile-popup")
+
+        function openPopup1(){
+            popup1.classList.add("open-popup");
+        }
+
+        function openPopup2(){
+            popup2.classList.add("open-popup");
+        }
+
+        function openPopup3(){
+            popup3.classList.add("open-popup");
+        }
+
+        function openPopup4(){
+            popup4.classList.add("open-popup");
+        }
+
+        function closePopup1(){
+            popup1.classList.remove("open-popup");
+        }
+
+        function closePopup2(){
+            popup2.classList.remove("open-popup");
+        }
+
+        function closePopup3(){
+            popup3.classList.remove("open-popup");
+        }
+
+        function closePopup4(){
+            popup4.classList.remove("open-popup");
+        }
+
+        function profilepopup(){
+          profilePopup.classList.add("open-profile")
+        }
+
+function closeprofilepopup(){
+            profilePopup.classList.remove("open-profile");
+        }
+
+
+        
+        function withdraw(){
+            popup5.classList.add("open-popup");
+        }
+        function closewithdraw(){
+            popup5.classList.remove("open-popup");
+        }
+
+
+                function error(){
+            window.location.href = "error.html";
+        }
+
+
+
+
+// DROPDOWN FOR SELECT BANK
+const dropdown = document.querySelector(".select-bank-dropdown");
+const input = document.getElementById("select-bank-dropdownInput");
+const menu = document.getElementById("dropdownMenu");
+const selectedValue = document.getElementById("selectedValue");
+
+input.addEventListener("click", () => {
+  dropdown.classList.toggle("open");
+});
+
+menu.querySelectorAll("li").forEach(item => {
+  item.addEventListener("click", () => {
+    selectedValue.textContent = item.dataset.value;
+    selectedValue.style.color = "#111";
+    dropdown.classList.remove("open");
+  });
+});
+
+/* Close when clicking outside */
+document.addEventListener("click", (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove("open");
+  }
+});
+
+    const planRadios = document.querySelectorAll('input[name="plan"]');
+const goalSection = document.getElementById('goalSection');
+
+planRadios.forEach(radio => {
+  radio.addEventListener('change', () => {
+    goalSection.style.display =
+      radio.value === 'goal' ? 'block' : 'none';
+  });
+});
+
+
+
+
+
+
+
+
---
+++ js/user.js
@@ -0,0 +1,136 @@
+const users = [ 
+  {
+    username: "mikel",
+    password: "odeyemi",
+    name: "Mikel Odeyemi",
+    balances: {
+      btc: 827199,
+      eth: 95623,
+      visa: 74384
+    },
+      amountType: {
+      right: "DOLLAR",
+      middle: "BTC",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Mikel's picture
+  },
+
+
+  {
+    username: "damosky",
+    password: "adebayo",
+    name: "Damola Adebayo",
+    balances: {
+      btc: 230450,
+      eth: 64890,
+      visa: 80000
+    },
+      amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  },
+
+   {
+    username: "lebs",
+    password: "lebile",
+    name: "Ayo Lebile",
+    balances: {
+      btc: 190450,
+      eth: 98890,
+      visa: 120000
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  },
+
+   {
+    username: "denzeli",
+    password: "esezobor",
+    name: "Denzel Esezobor",
+    balances: {
+      btc: 210450,
+      eth: 85890,
+      visa: 81000
+    },
+    amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+   image: "images/mikel.png" // path to Stella's picture
+  },
+
+   {
+    username: "rami",
+    password: "shoneye",
+    name: "Ramadan Shoneye",
+    balances: {
+      btc: 10450,
+      eth: 952890,
+      visa: 32000
+    },
+    amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  },
+
+   {
+    username: "ay",
+    password: "adeyemi",
+    name: "Ayo Adeyemi",
+    balances: {
+      btc: 100450,
+      eth: 452890,
+      visa: 22000
+    },
+          amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  },
+
+     {
+    username: "chase",
+    password: "elemide",
+    name: "gbenga Elemide",
+    balances: {
+      btc: 100450,
+      eth: 452890,
+      visa: 22000
+    },
+          amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  },
+
+
+       {
+    username: "flair",
+    password: "fawole",
+    name: "Tolu Fawole",
+    balances: {
+      btc: 100450,
+      eth: 452890,
+      visa: 22000
+    },
+          amountType: {
+      right: "BTC",
+      middle: "DOLLAR",
+      left: "EURO"
+    },
+    image: "images/mikel.png" // path to Stella's picture
+  }
+];
+
+// 👉 Add more users below anytime
