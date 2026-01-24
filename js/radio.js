f3445ee0a24d79a638eb4567c31e6ae169bc90e5
mikel-web <mikelodeyemi@gmail.com>
2026-01-24 18:51:40 +0100

    Delete js directory

--- js/radio.js
+++
@@ -1,9 +0,0 @@
-  const planRadios = document.querySelectorAll('input[name="plan"]');
-const goalSection = document.getElementById('goalSection');
-
-planRadios.forEach(radio => {
-  radio.addEventListener('change', () => {
-    goalSection.style.display =
-      radio.value === 'goal' ? 'block' : 'none';
-  });
-});
