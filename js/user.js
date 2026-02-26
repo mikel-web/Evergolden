40908f63b63f57468071cb4fb1d17855803779ca
mikel-web <mikelodeyemi@gmail.com>
2026-02-26 03:16:36 +0100

    Add files via upload

---
+++ js/user.js
@@ -0,0 +1,67 @@
+const users = [ 
+  {
+    username: "mikel",
+    password: "odeyemi",
+    name: "Mikel Odeyemi",
+    balances: {
+      btc: 827199,
+      eth: 95623,
+      visa: 74384
+    },
+
+    savingsByMonth: {
+      Jan: 29374,
+      Feb: 49631,
+      Mar: 14000,
+      Apr: 59095,
+      May: 57828,
+      Jun: 36684,
+      Jul: 33572,
+      Aug: 39974,
+      Sep: 48847,
+      Oct: 48116,
+      Nov: 61004,
+      Dec: 72000
+    },
+
+    savingprogress: {
+    saved: 900,
+    goal: 1200
+    },
+      amountType: {
+      right: "DOLLAR",
+      middle: "BTC",
+      left: "EURO"
+    },
+    image:{
+      right:"",
+      middle:"images/BTC.png",
+      left:""
+    },
+    userImage: "images/mikel.png", // path to Mikel's profile picture
+    userPlanlogo: "images/9165752.png",
+    userPlanName: "Retirement Fund",   // path to Mikel's plan logo
+
+    planDetails: {
+      period: "Long-Term Savings",
+      risk: "Low",
+      return: "+8-12% yearly"
+    },
+    
+    personalDetails: {
+      age: 21,
+      gender: "Male",
+      nationality: "Nigerian",
+      address: "7,adeola alabi street, peaceland estate, giwa-okearo, lagos state"
+    },
+
+    plan: "retirement", // current active plan
+    nok: "Tomiwa Odeyemi"
+  },
+
+  
+
+    
+];
+
+// 👉 Add more users below anytime
